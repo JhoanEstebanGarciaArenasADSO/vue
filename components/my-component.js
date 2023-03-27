6b436a4da5125ae869e2f64a6fe0490f5ad6e52f
JhoanEstebanGarciaArenasADSO <jegarcia155@misena.edu.co>
2023-03-26 22:10:25 -0500

    v-if-else

--- components/my-component.js
+++ components/my-component.js
@@ -1,7 +1,12 @@
 export default{
     data(){
         return{
-            centinela: false 
+            mensaje: `<em>Apolo</em>`,
+            mensaje2: `<mark>Artemis</mark>`,
+            mensaje3: `<u>Sputnik</u>`,
+            mensaje4: `No cumple la condicion`,
+            opcion: null,
+
         }
     }
 }
